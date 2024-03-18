1697bd31de6ea9a8f77eed29c2129eb3a150215f
Aaryan Kalra <aryan.smart832@gmail.com>
2024-03-18 13:25:14 +0530

    add dropdown profile

--- client/src/components/LoggedInUser.tsx
+++ client/src/components/LoggedInUser.tsx
@@ -1,30 +1,59 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useDispatch } from "react-redux";
 import { signOut } from "../redux/user/userSlice";
+import { useNavigate } from "react-router-dom";
 
 function LoggedInUser({ img }: { img: any }) {
   const [open, setOpen] = useState<boolean>(false);
   const dispatch = useDispatch();
+  const refer = useRef<HTMLDivElement>(null);
+  const navigate = useNavigate();
 
-  //   console.log(img);
+  function handle() {
+    setOpen(false);
+    dispatch(signOut());
+  }
+
+  useEffect(() => {
+    const closeDropDown = (e: MouseEvent) => {
+      if (!refer.current?.contains(e.target as Node)) {
+        setOpen(false);
+      }
+    };
+
+    document.body.addEventListener("click", closeDropDown);
+
+    return () => document.body.removeEventListener("click", closeDropDown);
+  }, []);
+  const handleProfile = () => {
+    setOpen(false);
+    navigate("/profile");
+  };
+
   return (
-    <div
-      className="relative bg-black rounded-full cursor-pointer"
-      onClick={() => setOpen(!open)}
-    >
+    <div ref={refer} className="relative bg-black rounded-full ">
       <img
+        onClick={() => setOpen(!open)}
         src={img.profilePicture}
-        className="w-7 rounded-full h-7"
+        className="cursor-pointer border-[2px] border-gray-500 w-8 rounded-full h-8"
         alt="display-img"
       />
       {open && (
-        <div className="z-[100]  absolute top-full flex flex-col items-start gap-2 bg-white w-[250px] h-fit right-1 rounded-lg shadow-lg">
-          <div className="border-b-2 p-2 w-full ">
+        <div className="z-[100]  absolute top-full flex flex-col items-start gap-2 bg-white w-[250px] h-fit right-1 rounded-lg shadow-lg overflow-hidden cursor-default">
+          <div className=" p-2 w-full ">
             <h1 className="text-gray-600">@{img.username}</h1>
             <h1 className="text-ellipsis overflow-hidden">{img.email}</h1>
           </div>
-          <h1 className="border-b-2 w-full px-2 py-1">Profile</h1>
-          <h1 className="px-2 py-1" onClick={() => dispatch(signOut())}>
+          <h1
+            onClick={handleProfile}
+            className="hover:bg-[#efefef] border-t-2 cursor-pointer duration-300   w-full px-2 py-2"
+          >
+            Profile
+          </h1>
+          <h1
+            className="hover:bg-[#efefef] border-t-2 cursor-pointer duration-300 w-full px-2 py-2"
+            onClick={handle}
+          >
             Sign out
           </h1>
         </div>
